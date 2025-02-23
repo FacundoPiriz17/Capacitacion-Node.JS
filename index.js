dbea4d49ea15f2eb2ee875d20fad536b77cb8c26
FacundoPiriz17 <facundo.piriz@correo.ucu.edu.uy>
2025-02-23 16:27:17 -0300

    finish

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const app = express();
 
 app.use(express.json());
 
-
-//const middlewares = jsonServer.defaults();
-//app.use(middlewares);
-
 app.use("/", routeUsuarios);
 app.listen((3000),()=>{
     console.log("Server is Running")
